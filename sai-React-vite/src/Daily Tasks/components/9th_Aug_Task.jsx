3830a079f648267886af00395a87a3193347bc4d
Saikiran-Avusula <saikiranavusula89@gmail.com>
2024-08-24 12:58:01 +0530

    update

---
+++ sai-React-vite/src/Daily Tasks/components/9th_Aug_Task.jsx
@@ -0,0 +1,8 @@
+// 1.What are the key differences between synchronous and asynchronous programming in JavaScript?
+
+// Synchronus programming:
+// - Javascript is called as single threaded synchronous language.
+// - mean that let's say an ex: a file concists of functions to perfrom some desired actions, here, synchrouns comes in play
+//   were a function is executed rest of functions has to wait for the completion of initial funciton to complete it's task assigned
+//   by this interpreter will be executed code in line by line.
+// - 
